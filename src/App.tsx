9a8b81fed01356277cde657599625b36ad81be6c
huyixi <hi@huyixi.com>
2025-02-19 16:18:20 +0800

    add shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { useState } from "react";
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1 className="text-3xl font-bold underline text-red-500">
         Hello world!
       </h1>
+      <Button>Button</Button>
     </>
   );
 }
